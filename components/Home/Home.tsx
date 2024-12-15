c60cf5db16e0e204e9356950d1f82408f8c24f94
mahdi sadeghi <mahdisadeghimsm1376@gmail.com>
2024-12-16 00:28:44 +0330

    property section

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './Hero/Hero'
-import Appartmenttype from './AppartmentType/Appartmenttype'
+import Appartmenttype from './AppartmentType/AppartmentType'
+import Properties from './Properties/Properties'
 
 const Home = () => {
   return (
     <div className='overflow-hidden '>
       <Hero/>
       <Appartmenttype/>
+      <Properties/>
     </div>
   )
 }
