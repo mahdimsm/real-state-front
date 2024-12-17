fe8b2281922cd3f4e26ce33d9232ccfbb79b0a70
mahdi sadeghi <mahdisadeghimsm1376@gmail.com>
2024-12-17 12:34:27 +0330

    edit min-h in Building Card

--- components/Home/BuildingFeature/BuildingFeatureCard.tsx
+++ components/Home/BuildingFeature/BuildingFeatureCard.tsx
@@ -12,7 +12,7 @@ type Props={
 }
 const BuildingFeatureCard = ({building}:Props) => {
   return (
-    <Tilt className='rounded-lg shadow-md p-4 bg-white'>
+    <Tilt className='rounded-lg shadow-md p-4 bg-white min-h-[240px] flex flex-col justify-between'>
         <div className='w-16 h-16 ml-auto flex items-center justify-center flex-col bg-gray-100 rounded-full'>
             <Image src={building.image} alt={building.title} width={40} height={40}/>
         </div>
