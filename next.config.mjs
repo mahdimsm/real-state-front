837ea2c912371a2e0826eb21b897d19e818d27b1
mahdi sadeghi <mahdisadeghimsm1376@gmail.com>
2025-01-02 23:58:12 +0330

    edit mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-module.exports = {
-    basePath: '/https://mahdimsm.github.io/real-state-front/', // Replace 'repository-name' with your actual repository name
-    images: {
-      unoptimized: true, // Required for Next.js on GitHub Pages (to disable Image Optimization)
-    },
-  };
+const nextConfig = {
+    //     basePath: '/https://mahdimsm.github.io/real-state-front/', // Replace 'repository-name' with your actual repository name
+//     images: {
+//       unoptimized: true, // Required for Next.js on GitHub Pages (to disable Image Optimization)
+//     },
+};
+// module.exports = {
+
+//   };
 export default nextConfig;
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  basePath: isProd ? '/repository-name' : '', // Use basePath only in production
+  images: {
+    unoptimized: true, // Required for GitHub Pages
+  },
+};
