59cc003e8eba7c8109d231830310bee095502a92
mahdi sadeghi <mahdisadeghimsm1376@gmail.com>
2025-01-02 23:41:19 +0330

    add basepath for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
+module.exports = {
+    basePath: '/https://mahdimsm.github.io/real-state-front/', // Replace 'repository-name' with your actual repository name
+    images: {
+      unoptimized: true, // Required for Next.js on GitHub Pages (to disable Image Optimization)
+    },
+  };
 export default nextConfig;
