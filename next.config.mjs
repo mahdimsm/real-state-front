bcd0428f2671ef152e3e7a7d28c4f57a5e5b96af
mahdi sadeghi <mahdisadeghimsm1376@gmail.com>
2025-01-03 00:24:51 +0330

    final edit mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    //     basePath: '/https://mahdimsm.github.io/real-state-front/', // Replace 'repository-name' with your actual repository name
-//     images: {
-//       unoptimized: true, // Required for Next.js on GitHub Pages (to disable Image Optimization)
-//     },
-};
-// module.exports = {
 
-//   };
-export default nextConfig;
+// Check if the app is running in production
 const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = {
-  basePath: isProd ? '/repository-name' : '', // Use basePath only in production
+// Export the Next.js configuration
+const nextConfig = {
+  // Dynamically set the basePath for GitHub Pages in production
+  basePath: isProd ? '/https://mahdimsm.github.io/real-state-front/' : '',
+
+  // Disable Next.js Image Optimization for GitHub Pages
   images: {
-    unoptimized: true, // Required for GitHub Pages
+    unoptimized: true,
   },
+
+  // Optional: Add other Next.js configurations here
 };
+
+export default nextConfig;
